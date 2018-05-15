1ab0522e5a9efdc8d4d5a280ca351dcf966a3fd8
MidouT <hamidou.traore@etud.univ-angers.fr>
2018-05-15 21:48:04 +0200

    Implémentation du système de lancement des section

---
+++ app/src/main/java/univ/m2acdi/apprentissageborel/activity/SectionIntroductActivity.java
@@ -0,0 +1,84 @@
+package univ.m2acdi.apprentissageborel.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import univ.m2acdi.apprentissageborel.R;
+import univ.m2acdi.apprentissageborel.util.TextSpeaker;
+
+public class SectionIntroductActivity extends Activity {
+
+    private final int SHORT_DURATION = 1000;
+
+    private int section;
+
+    private TextSpeaker textSpeaker;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_section_introduct);
+
+        section = getIntent().getExtras().getInt("section");
+        textSpeaker = (TextSpeaker) getIntent().getSerializableExtra("speaker");
+
+        Toast.makeText(this, "Section" + section, Toast.LENGTH_SHORT).show();
+
+        introductSection();
+
+    }
+
+
+
+    View.OnClickListener readListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            //speakOut();
+        }
+    };
+
+    private void introductSection() {
+
+        String text = "";
+
+        switch (section){
+            case 1:
+                text = this.getResources().getString(R.string.introduct_section_1);
+                break;
+            case 2:
+                text = this.getResources().getString(R.string.introduct_section_2);
+                break;
+            case 3:
+                text = this.getResources().getString(R.string.introduct_section_3);
+                break;
+
+                default:
+                    break;
+        }
+
+        speakOut(text);
+    }
+
+    protected void speakOut(String text) {
+        if (!textSpeaker.isSpeaking()) {
+            textSpeaker.speakText(text);
+            textSpeaker.pause(SHORT_DURATION);
+        }
+
+    }
+
+
+    @Override
+    protected void onStop() {
+
+        textSpeaker.destroy();
+        super.onStop();
+    }
+
+
+    public int getSection() {
+        return section;
+    }
+}
---
+++ app/src/main/java/univ/m2acdi/apprentissageborel/fragment/SectionIntroductFragment.java
@@ -0,0 +1,86 @@
+package univ.m2acdi.apprentissageborel.fragment;
+
+
+import android.app.Fragment;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.TextView;
+
+import univ.m2acdi.apprentissageborel.R;
+import univ.m2acdi.apprentissageborel.activity.SectionIntroductActivity;
+import univ.m2acdi.apprentissageborel.activity.TextToSpeechActivity;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class SectionIntroductFragment extends Fragment {
+
+    private TextView sectionIntroductTitle;
+    private ImageButton sectionStartBtn;
+
+
+    public SectionIntroductFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_section_introduct, container, false);
+
+        sectionIntroductTitle = view.findViewById(R.id.section_introduct_title);
+        sectionStartBtn = view.findViewById(R.id.section_activity_start_btn);
+        sectionStartBtn.setOnClickListener(sectionStartBtnClickListener);
+
+        setSectionTitle();
+
+        return view;
+    }
+
+    View.OnClickListener sectionStartBtnClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            Intent intent = new Intent();
+            int section = getActivity().getIntent().getExtras().getInt("section");
+            switch (section){
+                case 1:
+                    intent.setClass(getActivity().getBaseContext(), TextToSpeechActivity.class);
+                    break;
+                case 2:
+                    
+                    break;
+                case 3:
+                    break;
+                default:
+                    intent.setClass(getActivity().getBaseContext(), SectionIntroductActivity.class);
+                    break;
+            }
+            startActivity(intent);
+        }
+    };
+
+
+    protected void setSectionTitle() {
+
+        int section = getActivity().getIntent().getExtras().getInt("section");
+
+            switch (section){
+                case 1:
+                    sectionIntroductTitle.setText(getResources().getString(R.string.section_title_1));
+                    break;
+                case 2:
+                    sectionIntroductTitle.setText(getResources().getString(R.string.section_title_2));
+                    break;
+                case 3:
+                    sectionIntroductTitle.setText(getResources().getString(R.string.section_title_3));
+                    break;
+                default:
+                    sectionIntroductTitle.setText(getResources().getString(R.string.app_name));
+            }
+    }
+}
